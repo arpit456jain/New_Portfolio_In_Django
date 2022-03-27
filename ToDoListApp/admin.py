c8e07fd2fd98462e31ca0dd1978bafaddb6e669e
“Arpit <111arpit1@gmail.com>
2022-03-27 23:57:05 +0530

    update

--- ToDoListApp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from ToDoListApp.models import Task
-
-admin.site.register(Task)
--- ToDoListApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TodolistappConfig(AppConfig):
-    name = 'ToDoListApp'
